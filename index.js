8b54484326ebacecf9b2747d50046f0ad780626d
Wes Grimes <wesleygrimes@icloud.com>
2016-04-19 09:57:21 -0400

    add structure to module

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
-var moment = require("moment");
-require("twix");
+/*! 
+* rating-utils 
+* Copyright(c) 2016 Wes Grimes 
+* MIT Licensed 
+*/ 
 
-/**
- * Rating utils, library of helper methods for rating insurance policies
- * 
- * @param {String} policyEffectiveDate
- * @param {String} revisionEffectiveDate
- * @return {Float}
- */
-module.exports = {
-    proRataFactor: function(policyEffectiveDate, revisionEffectiveDate) {
-        var actualDays = moment(policyEffectiveDate).twix(revisionEffectiveDate).length("days");
-        var effPlusYear = moment(policyEffectiveDate).add(1, "years").subtract(1, "days");
-        var daysIfFullYear = moment(policyEffectiveDate).twix(effPlusYear).length("days");
-        return (actualDays/daysIfFullYear).toPrecision(4);
-    }
-};
+'use strict';
+
+module.exports = require('./lib/rating-utils');
